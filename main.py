936f2b66f6ed84052809f866d66024e85f4c2c2f
Bob Otter <bob.otter.100@gmail.com>
2017-07-14 19:44:09 +0000

    Resolved the unexpected tab

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import message
 def main():
     print "Hello CST438!"
     print random.randrange(-1, 10)
-	message.greeting()
+    message.greeting()
     
-	# Add your code here!!
-	print "Alice Otter did a contribution."
-	
-	print "Bye!"
+    # Add your code here!!
+    print "Alice Otter did a contribution."
+    
+    print "Bye!"
     
 main()
